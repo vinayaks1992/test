f2d69b57f3f9c13e0f35449b0b88d37a98b8ece1
PrathameshBelnekar <119807086+PrathameshBelnekar@users.noreply.github.com>
2023-01-08 17:01:49 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+app.app_context().push()
 
 
 class Todo(db.Model):
