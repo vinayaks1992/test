bb7725b4048eba067382af471379d2d2afed31d7
PrathameshBelnekar <119807086+PrathameshBelnekar@users.noreply.github.com>
2023-01-08 23:43:28 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect , url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,7 +17,36 @@ class Todo(db.Model):
 
 @app.route("/")
 def index():
-    return render_template('dashboard/index.html')
+    todo_list = Todo.query.all()
+    total_todo = Todo.query.count()
+    completed_todo = Todo.query.filter_by(complete=True).count()
+    uncompleted_todo = total_todo - completed_todo
+    return render_template('dashboard/index.html',todo_list=todo_list, total_todo=total_todo, completed_todo=completed_todo,uncompleted_todo=uncompleted_todo)
+
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    new_todo = Todo(title=title,complete = False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<int:id>')
+def update(id):
+    todo = Todo.query.filter_by(id=id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/about')
 def about():
